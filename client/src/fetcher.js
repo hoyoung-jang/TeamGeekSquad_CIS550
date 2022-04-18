d4c5af964c317e138436793d4f12542e8198d2a2
hoyoung <hoyoung.jang1990@gmail.com>
2022-04-18 21:54:29 +0900

    updated fetcher.js

--- client/src/fetcher.js
+++ client/src/fetcher.js
@@ -46,11 +46,44 @@ const getPlayerSearch = async (name, nationality, club, rating_high, rating_low,
 
 
 
+const getRestaurantByPostalCode = async (postal_code, page, pagesize) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/restaurant_by_postal_code?postal_code=${postal_code}&page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
 
 
+const getZipsForGoodMealsByType = async (meal_type, page, pagesize) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/zips_for_good_meals_by_type?postal_code=${meal_type}&page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
 
 
+const getFilterAttributes = async (state, bike_parking, wifi, page, pagesize) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/filter_attributes?state=${state}&bike_parking=${bike_parking}&wifi=${wifi}&page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
 
+
+const getFilterNeighborhoods = async (state, postal_code, meal_type, page, pagesize) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/filter_neighborhoods?state=${state}&postal_code=${postal_code}&meal_type=${meal_type}&page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+
+const getTopTenRestaurantsByCityCOVID = async (city, page, pagesize) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/top_ten_restaurants_by_city_COVID?city=${city}&page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
 
 
 
@@ -61,10 +94,10 @@ export {
     getPlayer,
     getMatchSearch,
     getPlayerSearch,
-    restaurant_by_postal_code,
-    zips_for_good_meals_by_type,
-    filter_attributes,
-    filter_neighborhoods,
-    calc_revisit_rate_by_business_id,
+
+    getRestaurantByPostalCode,
+    getZipsForGoodMealsByType,
+    getFilterAttributes,
+    getFilterNeighborhoods,
     top_ten_restaurants_by_city_COVID
 }
